c1c80c4b20dbc48ede1885b64a48802ee1d36926
Geraud WSL2 <geraud2201@gmail.com>
2025-01-29 13:30:45 +0200

    Checkpoint: implement NewInfluxDatafetcher

--- datafetcher/influx.go
+++ datafetcher/influx.go
@@ -1,1 +1,19 @@
 package datafetcher
+
+import (
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+)
+
+type InfluxDatafetcher struct {
+	db       influxdb2.Client
+	queryApi api.QueryAPI
+}
+
+func NewInfluxDatafetcher(org, url, token string) (*InfluxDatafetcher, error) {
+	db := influxdb2.NewClient(url, token)
+	return &InfluxDatafetcher{
+		db:       db,
+		queryApi: db.QueryAPI(org),
+	}, nil
+}
