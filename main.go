ee658524b0f36faa9d39876d77866cff6c52383f
Geraud WSL2 <geraud2201@gmail.com>
2025-01-29 15:31:25 +0200

    log started port accurately

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 		log.Fatalf("error initializing app: %v", err)
 	}
 	api.StartHttpServer()
-	log.Println("Started server on :8086")
+	log.Printf("Started server on %s", c.GetString("Port"))
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
