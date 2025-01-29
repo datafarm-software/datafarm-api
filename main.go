204e2caa341a823f8ae859781dbb5689a3e6d81d
Geraud WSL2 <geraud2201@gmail.com>
2025-01-29 10:14:29 +0200

    Checkpoint: Start HTTP server with graceful shutdown

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	apiModule "github.com/geraud22/aquahaus-api/api"
+)
+
+func main() {
+	opts, err := apiModule.NewApiOpts(":8086")
+	if err != nil {
+		log.Fatalf("error getting api opts: %v", err)
+	}
+	api, err := apiModule.NewApi(opts)
+	if err != nil {
+		log.Fatalf("error initializing app: %v", err)
+	}
+	api.StartHttpServer()
+	log.Println("Started server on :8086")
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Received shutdown signal...")
+	api.Shutdown()
+	log.Println("Graceful shutdown complete.")
+}
