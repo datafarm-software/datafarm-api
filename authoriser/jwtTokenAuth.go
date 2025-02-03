efa978f0ac9c7017d6c475ea035d735304a1a09d
Geraud WSL2 <geraud2201@gmail.com>
2025-02-03 10:16:00 +0200

    create jwtTokenAuth.go and redisBasicAuth.go under authoriser package

--- authoriser/jwtTokenAuth.go
+++ authoriser/jwtTokenAuth.go
@@ -1,51 +1,16 @@
 package authoriser
 
 import (
-	"context"
 	"crypto/ecdsa"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
 	"io"
 	"io/fs"
-	"log"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/redis/go-redis/v9"
-	"golang.org/x/crypto/bcrypt"
 )
-
-var g_ctx = context.Background()
-
-type redisBasicAuth struct {
-	db *redis.Client
-}
-
-func ConnectRedis(addr, passw string, db int) *redis.Client {
-	client := redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: passw,
-		DB:       db,
-	})
-	if _, err := client.Ping(g_ctx).Result(); err != nil {
-		log.Fatalf("Error connecting to Redis client: %v", err)
-	}
-	return client
-}
-
-func NewRedisBasicAuth(addr, password string, db int) *redisBasicAuth {
-	return &redisBasicAuth{
-		db: ConnectRedis(addr, password, db),
-	}
-}
-
-func (r *redisBasicAuth) Close() error {
-	if err := r.db.Close(); err != nil {
-		return fmt.Errorf("error closing redis client: %v", err)
-	}
-	return nil
-}
 
 type jwtAuth struct {
 	publicKey  *ecdsa.PublicKey
@@ -130,19 +95,4 @@ func (j *jwtAuth) GenerateToken() (string, error) {
 		return "", err
 	}
 	return tokenString, nil
-}
-
-func (r *redisBasicAuth) CheckCredentials(username, passw string) error {
-	uuid, err := r.db.Get(g_ctx, "unique:"+username).Result()
-	if err != nil {
-		return fmt.Errorf("error getting uuid for username %s: %v", username, err)
-	}
-	passWordHash, err := r.db.HGet(g_ctx, "user:"+uuid, "password").Result()
-	if err != nil {
-		return fmt.Errorf("error getting password for username %s: %v", username, err)
-	}
-	if err := bcrypt.CompareHashAndPassword([]byte(passWordHash), []byte(passw)); err != nil {
-		return fmt.Errorf("passwords did not match: %v", err)
-	}
-	return nil
 }
