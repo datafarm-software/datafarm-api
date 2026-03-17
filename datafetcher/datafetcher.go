37cffdde509d16fb1a9786d0ca3b49f104f66aff
Geraud <geraud2201@gmail.com>
2026-03-17 15:15:42 +0200

    allow users to request multiple query fields in one request

--- datafetcher/datafetcher.go
+++ datafetcher/datafetcher.go
@@ -7,9 +7,9 @@ import (
 )
 
 type DeviceDataRequest struct {
-	QueryField string `json:"queryField" required:"true"`
-	Start      string `json:"start" required:"true"`
-	Stop       string `json:"stop" required:"false"`
+	QueryFields []string `json:"queryFields" required:"true"`
+	Start       string   `json:"start" required:"true"`
+	Stop        string   `json:"stop" required:"false"`
 }
 
 type ConsolidatedDeviceData struct {
