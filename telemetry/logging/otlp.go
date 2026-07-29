729953a3f2e5e2116e0c63cccd9af55c95ebe1b6
Geraud <geraud2201@gmail.com>
2026-07-29 12:20:38 +0200

    implement NewOtlpLogger

--- telemetry/logging/otlp.go
+++ telemetry/logging/otlp.go
@@ -1,3 +1,30 @@
 package logging
 
-type OtlpLogger struct{}
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/contrib/bridges/otelzap"
+	"go.opentelemetry.io/contrib/exporters/autoexport"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.uber.org/zap"
+)
+
+type OtlpLogger struct {
+	*zap.Logger
+}
+
+func NewOtlpLogger() (*OtlpLogger, error) {
+	o := &OtlpLogger{}
+	ctx := context.Background()
+	otlpexp, err := autoexport.NewLogExporter(ctx)
+	if err != nil {
+		return o, fmt.Errorf("newLogExporter: %v", err)
+	}
+	lp := log.NewLoggerProvider(
+		log.WithProcessor(log.NewBatchProcessor(otlpexp)),
+	)
+	o.Logger = zap.New(otelzap.NewCore("datafarm-rest-go",
+		otelzap.WithLoggerProvider(lp)))
+	return o, nil
+}
