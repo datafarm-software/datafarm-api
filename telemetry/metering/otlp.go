df3a2a1f662c7ec1bb79f623e4b5fc232bb08356
Geraud <geraud2201@gmail.com>
2026-07-30 10:30:53 +0200

    implement newotlpmeter func

--- telemetry/metering/otlp.go
+++ telemetry/metering/otlp.go
@@ -1,3 +1,21 @@
 package metering
 
-type OtlpMetering struct{}
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+)
+
+func NewOtlpMeter(res *resource.Resource) (*metric.MeterProvider, error) {
+	exporter, err := otlpmetrichttp.New(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("init exporter: %v", err)
+	}
+	reader := metric.NewPeriodicReader(exporter)
+	mp := metric.NewMeterProvider(
+		metric.WithReader(reader), metric.WithResource(res))
+	return mp, nil
+}
