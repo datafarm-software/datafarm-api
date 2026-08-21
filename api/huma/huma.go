6748c93bbef981581ceb3ff1ab6a72d2a12eaeb6
Geraud <geraud2201@gmail.com>
2026-08-21 15:53:30 +0200

    use the git repo in CI as well

--- api/huma/huma.go
+++ api/huma/huma.go
@@ -271,13 +271,12 @@ func RegisterHumaOperations(api huma.API, ho HumaOperator) {
 
 func Config(mode Mode) (config huma.Config) {
 	var cmd *exec.Cmd
-	switch mode {
-	case Development:
-		cmd = exec.Command("git", "describe", "--tags", "--abbrev=0")
-	default:
-		cmd = exec.Command("cat", "/app/.git-version")
-	}
+	cmd = exec.Command("git", "describe", "--tags", "--abbrev=0")
 	version, _ := cmd.Output()
+	if version == nil {
+		cmd = exec.Command("cat", "/app/.git-version")
+		version, _ = cmd.Output()
+	}
 	config = huma.DefaultConfig("DataFarm SensorData API", string(version))
 	config.Info.Description = `
 ## Welcome
